3395d142d81aad5a4609892a3a9b25bc5f2858ac
yoyo383 <yoad.winter@gmail.com>
2023-12-08 22:11:00 +0200

    added protocol and server functions

---
+++ protocol.py
@@ -0,0 +1,42 @@
+import socket
+import struct
+
+PACK_FORMAT = 'I'
+LENGTH_SIZE = 4
+
+
+def send_data(sock, data):
+    while data:
+        count = sock.send(data)
+        data = data[count:]
+
+
+def send(sock, cmd, args):
+    cmd_len = struct.pack(PACK_FORMAT, socket.htonl(len(cmd)))
+    args_str = str(args)
+    args_len = struct.pack(PACK_FORMAT, socket.htonl(len(args_str)))
+    data = cmd_len + cmd.encode() + args_len + args_str.encode()
+    send_data(sock, data)
+
+
+def try_receive(sock, length):
+    result = b''
+    while len(result) < length:
+        received = sock.recv(length - len(result))
+        if not received:
+            result = b''
+            break
+        result += received
+    return result
+
+
+def receive_part(sock):
+    len_bytes = try_receive(sock, LENGTH_SIZE)
+    len_int = socket.ntohl(struct.unpack(PACK_FORMAT, len_bytes)[0])
+    return try_receive(sock, len_int).decode()
+
+
+def receive(sock):
+    cmd = receive_part(sock)
+    data = receive_part(sock)
+    return [cmd, data]
---
+++ server_funcs.py
@@ -0,0 +1,50 @@
+import glob
+import os
+import shutil
+import subprocess
+import pyautogui
+
+
+def func_dir(path):
+    if not os.path.isdir(path):
+        return f"'{path}' is not a directory."
+    files = glob.glob(f'{path}/*.*')  # using / instead of \ so it could also work on Mac and Linux.
+    return '\n'.join(files)
+
+
+def func_delete(path):
+    if not os.path.isfile(path):
+        return f"'{path}' does not exist."
+    # since I know that path must exist (because of the if statement), I don't need to try/except.
+    os.remove(path)
+    return f"Deleted '{path}'."
+
+
+def func_copy(source, destination):
+    if not os.path.isfile(source):
+        return f"'{source}' does not exist."
+    try:
+        shutil.copy(source, destination)
+        return f"Successfully copied '{source}' into '{destination}'."
+    except (PermissionError,  shutil.SameFileError):
+        return f"Failed to copy '{source}' into '{destination}'."
+
+
+def func_execute(path):
+    if not os.path.isfile(path):
+        return f"'{path}' does not exist."
+    try:
+        subprocess.call(path)
+        return f"Successfully executed '{path}'."
+    except OSError:
+        return f"Could not execute '{path}'."
+
+
+def func_screenshot():
+    image = pyautogui.screenshot()
+    image.save('image.jpg')
+    return str((image.mode, image.size, image.tobytes()))
+
+
+def func_exit():
+    return 'Exiting.'
